e339f4a0aa4c13a2cdd47d3d1e076df4f32e4d28
Ammar Ahmad <61749743+aahmad-dev@users.noreply.github.com>
2022-08-22 10:18:55 -0400

    Project files from Salesforce Platform

---
+++ expensesApp.app
@@ -0,0 +1,4 @@
+<aura:application extends="force:slds" implements="force:appHostable">
+        <!-- This component is the real "app" -->
+        <c:expenses/>
+</aura:application>
---
+++ expensesHelper.js
@@ -0,0 +1,32 @@
+({
+    createExpense: function(component, expense) {
+        let action = component.get("c.saveExpense");
+        action.setParams({
+            "expense": expense
+        });
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let expenses = component.get("v.expenses");
+                expenses.push(response.getReturnValue());
+                component.set("v.expenses", expenses);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    createExpense: function(component, expense) {
+        let action = component.get("c.saveExpense");
+        action.setParams({
+            "expense": expense
+        });
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let expenses = component.get("v.expenses");
+                expenses.push(response.getReturnValue());
+                component.set("v.expenses", expenses);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+})
